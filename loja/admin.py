7a36f53a58341c76d7998fc460089fa61980330a
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-16 19:05:40 -0300

    Area admin do django

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Produto)
+admin.site.register(Categoria)
+admin.site.register(Tipo)
+admin.site.register(ItemEstoque)
+admin.site.register(Pedido)
+admin.site.register(ItensPedido)
+admin.site.register(Endereco)
+admin.site.register(Cliente)
 
 # Register your models here.
