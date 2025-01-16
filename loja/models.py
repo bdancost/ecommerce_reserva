b631440254319e91b5f6f431649a4f1537630e71
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-01-16 17:46:03 -0300

    Criando uma classe de um modelo no banco de dados

--- loja/models.py
+++ loja/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Cliente(models.Model):
+  nome = models.CharField(max_length=200, null=True, blank=True)
+  email = models.CharField(max_length=200, null=True, blank=True)
+  telefone = models.CharField(max_length=200, null=True, blank=True)
+  id_sessao = models.CharField(max_length=200, null=True, blank=True)
+  usuario = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 
